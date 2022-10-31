6614a826316c2fb1fb51c88c47a655612054c9e1
JAM <electrojam@hotmail.com>
2022-10-31 16:25:21 -0500

    BE-4 Users

--- App.js
+++ App.js
@@ -1,13 +1,53 @@
+import { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigator from './src/navigation';
-import { Amplify } from 'aws-amplify'
+import { Amplify, API, Auth, graphqlOperation } from 'aws-amplify'
 import { withAuthenticator } from 'aws-amplify-react-native'
 import awsconfig from './src/aws-exports'
+import { getUser } from "./src/graphql/queries"
+import { createUser } from "./src/graphql/mutations"
 
 Amplify.configure({ ...awsconfig, Analytics: { disabled: true }})
 
 function App() {
+
+  useEffect(() => {
+    const syncUser = async () => {
+      //get Auth user
+      const authUser = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      })
+      console.log(authUser)
+
+      // query the database using user id (sub).  Busca si authUser está en DB de aws      
+      const userData = await API.graphql(
+        graphqlOperation(getUser, { id: authUser.attributes.sub })
+      )
+      console.log(userData)
+
+      if (userData.data.getUser) {
+        console.log("User already exists in DB") 
+        return
+      }
+
+      // if there isno users in db, create one
+      const newUser = {
+        id: authUser.attributes.sub,
+        name: authUser.attributes.phone_number,
+        status: 'Hey, I am using Whatsapp',
+      }
+
+      const newUserResponse = await API.graphql(
+        graphqlOperation(createUser, { input: newUser })
+      )
+
+    }
+
+    syncUser()
+  }, [])
+  
+
   return (
     <View style={styles.container}>
       <Navigator />
